2992995ea32fb8b2e75f799d16c2cfc90eff1c88
Lucas Nakano <lucas_nakano@hotmail.com>
2021-10-08 09:15:05 -0300

    Initial Commit

---
+++ AnaliseSentimento/AnaliseSentimentoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnaliseSentimentoApp.swift
+//  AnaliseSentimento
+//
+//  Created by Lucas Yoshio Nakano on 08/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AnaliseSentimentoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnaliseSentimento/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnaliseSentimento
+//
+//  Created by Lucas Yoshio Nakano on 08/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
