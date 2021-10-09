5092b1f6fc4190cef469e796e5c69d22aaf59423
Lucas Nakano <lucas_nakano@hotmail.com>
2021-10-09 11:35:26 -0300

    Tweet Sentiment Score

--- AnaliseSentimento/Service/TweetsService.swift
+++ AnaliseSentimento/Service/TweetsService.swift
@@ -6,9 +6,6 @@ final class TweetsService {
         let endpoint = TweetsEndpoint.fromUser(user)
         let url = endpoint.url
         let headers = endpoint.headers
-//        let encodedURL = "from:\(user)".addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-//        let url = "https://api.twitter.com/2/tweets/search/recent?query=\(encodedURL)"
-//        let headers: HTTPHeaders = ["Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAOv1UQEAAAAA3MLr1C7mtA7pquHZJnk5mGLWXug%3DelLUznFpVNW7BuuSBLuODqmO1DFrlCq8eYWj56i5zvGd6E9fLu"]
         AF.request(url, headers: headers)
             .validate()
             .responseData { response in
@@ -16,10 +13,10 @@ final class TweetsService {
                 case .success:
                     print("Fetch Tweets Validation Successful")
                 case let .failure(error):
-                    fatalError("Error Fetching Tweets: \(error)")
+                    print("Error Fetching Tweets: \(error)")
                 }
             }
-            .responseDecodable(of: RootResponse.self) { response in
+            .responseDecodable(of: RootResponse.self, queue: .main) { response in
                 guard let userTweets = response.value else { return }
                 completion(userTweets.data)
             }
