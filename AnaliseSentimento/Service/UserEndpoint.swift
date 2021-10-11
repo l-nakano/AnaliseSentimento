844c87180acd7d8710373ef0273270a7b84b5054
Lucas Nakano <lucas_nakano@hotmail.com>
2021-10-11 15:03:23 -0300

    Store Token

--- AnaliseSentimento/Service/UserEndpoint.swift
+++ AnaliseSentimento/Service/UserEndpoint.swift
@@ -15,7 +15,7 @@ extension UserEndpoint {
     }
     
     var headers: HTTPHeaders {
-        return ["Authorization": "Bearer AAAAAAAAAAAAAAAAAAAAAOv1UQEAAAAA3MLr1C7mtA7pquHZJnk5mGLWXug%3DelLUznFpVNW7BuuSBLuODqmO1DFrlCq8eYWj56i5zvGd6E9fLu"]
+        return ["Authorization": ProcessInfo.processInfo.environment["BEARER_TOKEN"]!]
     }
     
     static func fromUser(_ user: String, createdAt: Bool = false, location: Bool = false, description: Bool = false) -> Self {
