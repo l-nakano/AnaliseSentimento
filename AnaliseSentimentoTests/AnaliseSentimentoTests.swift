e6d8b880df180bf4fd46fc57e642b2508445296c
Lucas Nakano <lucas_nakano@hotmail.com>
2021-10-19 22:05:46 -0300

    API Test Connection

--- AnaliseSentimentoTests/AnaliseSentimentoTests.swift
+++ AnaliseSentimentoTests/AnaliseSentimentoTests.swift
@@ -1,33 +1,70 @@
-//
-//  AnaliseSentimentoTests.swift
-//  AnaliseSentimentoTests
-//
-//  Created by Lucas Yoshio Nakano on 08/10/21.
-//
-
 import XCTest
+import Alamofire
 @testable import AnaliseSentimento
 
 class AnaliseSentimentoTests: XCTestCase {
+    
+    var sut: Session!
+    let networkMonitor = NetworkMonitor.shared
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = AF
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testUserEndpointSuccessfulConnection() throws {
+        try XCTSkipUnless(networkMonitor.isReachable, "Network connectivity needed for this test")
+        
+        // Dado
+        let twitterUserEndpoint = TwitterEndpoint.twitterUser("Twitter")
+        let url = twitterUserEndpoint.url
+        let headers = twitterUserEndpoint.headers
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // Quando
+        sut.request(url, headers: headers)
+            .response { httpResponse in
+                statusCode = httpResponse.response?.statusCode
+                responseError = httpResponse.error
+                promise.fulfill()
+            }
+        wait(for: [promise], timeout: 5)
+        
+        // Então
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testTwittsEndpointSuccessfulConnection() throws {
+        try XCTSkipUnless(networkMonitor.isReachable, "Network connectivity needed for this test")
+        
+        // Dado
+        let twitterUserEndpoint = TwitterEndpoint.userTwitts(from: "Twitter")
+        let url = twitterUserEndpoint.url
+        let headers = twitterUserEndpoint.headers
+        let promise = expectation(description: "Completion handler invoked")
+        var statusCode: Int?
+        var responseError: Error?
+        
+        // Quando
+        sut.request(url, headers: headers)
+            .response { httpResponse in
+                statusCode = httpResponse.response?.statusCode
+                responseError = httpResponse.error
+                promise.fulfill()
+            }
+        wait(for: [promise], timeout: 5)
+        
+        // Então
+        XCTAssertNil(responseError)
+        XCTAssertEqual(statusCode, 200)
     }
 
 }
